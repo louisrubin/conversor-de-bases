c308814af8e74b73338b729c58f75d84ce77e062
louisrubin <miguel10.rbn@gmail.com>
2021-10-25 13:52:07 -0300

    muchos cambios. error: funcion 'decimal_a_hexa' retorna el valor invertido al que deria ser

--- conversiones_main.py
+++ conversiones_main.py
@@ -3,6 +3,60 @@ import os
 import re
 cls = lambda: os.system("cls")   # lambda sirve para crear funciones pequeñas y "anónimas", actúa como un "def"
 
+def ifs_menu(numero, base_origen, base_a_convertir):
+    numero = numero.upper()
+
+    if base_origen == base_a_convertir:
+        print (" [",numero,"] EN BASE ", base_origen, " =  [",numero,"] EN BASE ",base_a_convertir)
+        print()
+
+
+    elif base_a_convertir == 10:
+
+        if base_origen > 10 and base_origen <= 16:   # si el num ingr. está entre 11 y 16, usa "hexa_a_decimal()"
+            decimal = hexa_a_decimal(numero, base_origen)
+        else:
+            entero = string_a_entero(numero)        # saca el guion del negativo para retornarlo como entero
+            decimal = baseX_a_b10(entero, base_origen)
+
+        numero.upper()
+        print (" [",numero,"] EN BASE ", base_origen, " =  [",decimal,"] EN BASE ",base_a_convertir)
+        print()
+
+
+    elif base_a_convertir >= 11 and base_a_convertir <= 16:
+
+        if base_origen <= 10:
+            entero = string_a_entero(numero)
+            decimal = baseX_a_b10(entero, base_origen)
+            hexa = decimal_a_hexa(decimal, base_a_convertir)
+        else:
+            decimal = hexa_a_decimal(numero, base_origen)
+            hexa = decimal_a_hexa(decimal, base_a_convertir)
+
+        numero.upper()   # UPPERCASE
+        print (" [",numero,"] EN BASE ", base_origen, " =  [",hexa,"] EN BASE ",base_a_convertir)
+        print()
+
+    elif base_a_convertir > 1 and base_a_convertir < 10:
+
+        if base_origen >= 11:
+            decimal = hexa_a_decimal(numero, base_origen)
+            convertido = decimal_a_baseX(decimal, base_a_convertir)
+        else:
+            entero = string_a_entero(numero)
+            decimal = baseX_a_b10(entero, base_origen)
+            convertido = decimal_a_baseX(decimal, base_a_convertir)
+
+        numero.upper()
+        print (" [",numero,"] EN BASE ", base_origen, " =  [",convertido,"] EN BASE ",base_a_convertir)
+        print()
+
+    else: 
+        print(" - 'ERROR 404 - 087' -\n")
+        print()
+
+
 def baseX_a_b10(numero_ingresado, base_origen):
     """ conversor de X base  a DECIMAL """
     posicion = 0
@@ -16,6 +70,7 @@ def baseX_a_b10(numero_ingresado, base_origen):
         numero_ingresado *= -1   # si el num ingresado es negativo, lo convierte a positivo para poder operar
 
     numero_ingresado = str(numero_ingresado)[::-1]
+
     for digito in numero_ingresado:
         # Elevar X a la posición actual
         multiplicador = int(base_origen**posicion)
@@ -61,7 +116,6 @@ def decimal_a_hexa(decimal, base_a_convertir):
 
     # aca se crea el diccionario con los valores de ABC...
     for x in range(0, base_a_convertir - 10):
-        #              
         equivalencias[ x + 10 ] = chr(ord('A') + x)
 
     if decimal < 0:
@@ -76,74 +130,71 @@ def decimal_a_hexa(decimal, base_a_convertir):
         else:
             caracter_hex = str(residuo)
         decimal = int(decimal / base_a_convertir)
-        hex = caracter_hex + hex
+        hex += caracter_hex
 
     if negativo:
         return "-" + hex   # str  
     else:
         return hex
 
-def hexa_a_decimal(caracteres, base_origen):
-    """ variables
-                < funcion hecha por Marcos Pereyra con algunos cambios mios > """
+def hexa_a_decimal(valor, base_origen):
+    """ funcion como su nombre lo indica, hexadecimal a decimal
+        fuente: https://www.geeksforgeeks.org/program-for-hexadecimal-to-decimal/
+        """
 
-    negativo = False
-    for x in re.finditer("-", caracteres):        # re.finditer (cadena o caracter a buscar, cadena en donde buscar)
-        if x:                  
-            negativo = True         # si en el FOR encuentra el caracter buscado retorna True y rompe el ciclo
-            break
+    valor = valor.upper()
+    negativo = if_hexa_is_negative(valor)
 
+    length = len(valor)
 
-    if negativo:
-        caracteres = caracteres.replace("-", "")  # si "negativo" = True  quita "-" pero sigue siendo str
-
-    sumador = -1
+    base = 1
     decimal = 0
-    equivalencias = {}
-    caracteres = caracteres.upper()
 
-    """aca se crea el diccionario con los valores de ABC..."""
-    for x in range(0, base_origen - 10):
-        equivalencias[(chr(ord('A') + x))] = x + 10
+    for x in range(length -1, -1, -1):
+        if valor[x] >= '0' and valor[x] <= '9':
+            decimal += (ord(valor[x]) -48) * base
+        
+            base = base * base_origen
 
-    """este sumador es para elevar el 16^sumador, osea, la posicion en la que se encuentre"""
-    for x in caracteres:
-        sumador += 1
+        elif valor[x] >= 'A' and valor[x] <= 'F':
+            decimal += (ord(valor[x]) -55) * base
 
-    """aca ocurre la magia :v, aca transforma de hexa a decimal uwu"""
-    for x in caracteres:
-        if x in equivalencias:
-            decimal += equivalencias[x] * pow(base_origen, sumador)
-            sumador -= 1
-
-        elif x not in equivalencias:
-            pass
-
-        else:
-            decimal += int(x) * pow(base_origen, sumador)
-            sumador -= 1
-
+            base = base * base_origen
+            
     if negativo:
-        return (decimal) * -1      # si "negativo" = True convierte en negativo a la variable, TERMINA EN ENTERO
+        return decimal * -1
     else:
-        return decimal   # int
-
+        return decimal
 
 def string_a_entero(cadena):
-    """ funcion para quitar el caracter "-" de una CADENA de numeros y retornarlo como entero """
+    """ funcion para quitar el caracter "-" del negativo de una CADENA de numeros y retornarlo como entero """
+    negativo = if_hexa_is_negative(cadena)      # si la cadena ingresada tiene '-' o sea negativo
+
+    if negativo:
+        cadena = cadena.replace("-", "")        # quita el '-' y lo vuelve entero
+        return int(cadena) * -1
+    else:
+        return int(cadena)
+
+def if_hexa_is_negative(cadena):
     negativo = False
     for x in re.finditer("-", cadena):        # re.finditer (cadena o caracter a buscar, cadena en donde buscar)
         if x:                  
             negativo = True         # si en el FOR encuentra el caracter buscado retorna True y rompe el ciclo
             break
+    return negativo
 
-    if negativo:
-        cadena = cadena.replace("-", "")
-        cadena = int(cadena) * -1
-        return cadena
-    else:
-        cadena = int(cadena)
-        return cadena
+
+
+
+
+
+if __name__ == '__main__':
+    we = hexa_a_decimal(valor= '-ff', base_origen=16)
+    print(we)
+    print(type(we))
+
+
 
 
 """
--- experimentando.py
+++ experimentando.py
@@ -5,4 +5,47 @@ import os
 cls = lambda: os.system("cls")
 cls()
 
+def hexa_a_decimal(caracteres, base_origen):
+    """ variables
+                < funcion hecha por Marcos Pereyra con algunos cambios mios > """
 
+    negativo = False
+    for x in re.finditer("-", caracteres):        # re.finditer (cadena o caracter a buscar, cadena en donde buscar)
+        if x:                  
+            negativo = True         # si en el FOR encuentra el caracter buscado retorna True y rompe el ciclo
+            break
+
+
+    if negativo:
+        caracteres = caracteres.replace("-", "")  # si "negativo" = True  quita "-" pero sigue siendo str
+
+    sumador = -1
+    decimal = 0
+    equivalencias = {}
+    caracteres = caracteres.upper()
+
+    """aca se crea el diccionario con los valores de ABC..."""
+    for x in range(0, base_origen - 10):
+        equivalencias[(chr(ord('A') + x))] = x + 10
+
+    """este sumador es para elevar el 16^sumador, osea, la posicion en la que se encuentre"""
+    for x in caracteres:
+        sumador += 1
+
+    """aca ocurre la magia :v, aca transforma de hexa a decimal uwu"""
+    for x in caracteres:
+        if x in equivalencias:
+            decimal += equivalencias[x] * pow(base_origen, sumador)
+            sumador -= 1
+
+        elif x not in equivalencias:
+            pass
+
+        else:
+            decimal += int(x) * pow(base_origen, sumador)
+            sumador -= 1
+
+    if negativo:
+        return (decimal) * -1      # si "negativo" = True convierte en negativo a la variable, TERMINA EN ENTERO
+    else:
+        return decimal   # int
